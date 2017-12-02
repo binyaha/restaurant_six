2979ddb9ecfd8ae46374cc012f321b1b03a36d98
binyaha <yahabin@gmail.com>
2017-12-03 03:32:50 +0800

    完成show功能

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,9 +1,22 @@
 class Admin::RestaurantsController < ApplicationController
 
   before_action :check_admin
+  before_action :find_restaurant, only: [:show]
+
 
   def index
     @restaurants=Restaurant.all
+  end
+
+  def show
+  end
+
+
+
+  private
+
+  def find_restaurant
+    @restaurant=Restaurant.find(params[:id])
   end
 
 end
