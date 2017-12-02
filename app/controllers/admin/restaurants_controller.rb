c279b67abc369b84a4b2f3937a1fdbe07eba4d7f
binyaha <yahabin@gmail.com>
2017-12-03 03:53:26 +0800

    完成create功能

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -12,12 +12,29 @@ class Admin::RestaurantsController < ApplicationController
   @restaurant=Restaurant.new
  end
 
+ def create
+  @restaurant=Restaurant.new(restaurant_params)
+
+  if @restaurant.save
+    flash[:notice]= "新增成功"
+    redirect_to admin_restaurants_path
+  else
+    flash[:alert]= "新增失敗"
+    render :new
+  end
+ end
+
+
 
 
   private
 
   def find_restaurant
     @restaurant=Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name,:tel,:address,:opening_hours,:description)
   end
 
 end
