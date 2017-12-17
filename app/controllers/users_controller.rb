9cb051a1d9729800987f0bbff28c20688598c425
binyaha <yahabin@gmail.com>
2017-12-17 18:17:03 +0800

    完成profile 以評論餐廳功能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user=User.find(params[:id])
+    @commented_restaurants=@user.restaurants.sort
+    @temp={}
+    
   end
 
   def edit
